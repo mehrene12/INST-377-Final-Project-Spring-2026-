99951650b7aaded207fc18e848f1156e75902a9f
Mehrene Ullah <mehrene.ullah@gmail.com>
2026-05-17 20:01:34 -0400

    addvercel config and fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const supabaseClient = require('@supabase/supabase-js');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
